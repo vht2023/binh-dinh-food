5148fa5e6f3bcbfdc4a9ddbd3516825608a9c6a0
vhthien611 <thien.vh611@gmail.com>
2024-07-02 10:17:41 +0700

    Update Sitemap

--- sitemap.ts
+++ sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: 'https://haisanphoikho.vercel.app',
+			url: 'https://www.haisanphoikho.com',
 			lastModified: new Date(),
 		},
 	];
